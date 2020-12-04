cbe432d5384f7d69f726625350ab82175466a59d
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-12-04 19:54:42 +0100

    add setup and requirements

---
+++ setup.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cookiecutter-publication',
+    packages=[],
+    version='0.1.0',
+    description='Cookiecutter template for publications',
+    author='Marco Berzborn',
+    license='MIT',
+    author_email='marco.berzborn@akustik.rwth-aachen.de',
+    url='https://github.com/mberz/cookiecutter-publication',
+    keywords=['cookiecutter', 'template'],
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Scientists',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)

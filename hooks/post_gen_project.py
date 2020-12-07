1ab8c786b4bc1667176154ddce520aa0afbe9478
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-12-07 21:46:17 +0100

    post gen hook for manuscript

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import os
+from os.path import split
+import shutil
+import urllib3
+import zipfile
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def download_file(url, filename):
+    """Download a file
+    """
+    http = urllib3.PoolManager()
+    with open(filename, 'wb') as out:
+        r = http.request('GET', url, preload_content=False)
+        shutil.copyfileobj(r, out)
+
+
+def download_and_extract(url, filename):
+    """Download and extract the manuscript template
+    """
+    http = urllib3.PoolManager()
+    with open(filename, 'wb') as out:
+        r = http.request('GET', url, preload_content=False)
+        shutil.copyfileobj(r, out)
+
+    with zipfile.ZipFile(fname, 'r') as zip_ref:
+        fname_extract = os.path.join(
+            os.path.split(fname)[0], 'template')
+        zip_ref.extractall(fname_extract)
+
+
+def remove_file(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+def remove_dir(filepath):
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+if __name__ == '__main__':
+
+    if '{{ cookiecutter.manuscript }}' == 'n':
+        shutil.rmtree(os.path.join(
+            PROJECT_DIRECTORY, 'process', 'manuscript'))
+        shutil.rmtree(os.path.join(
+            PROJECT_DIRECTORY, 'manuscript'))
+    elif '{{ cookiecutter.manuscript }}' == 'JASA':
+        fname = os.path.join(PROJECT_DIRECTORY, 'manuscript', 'JASA.zip')
+        download_and_extract(
+            'https://acousticalsociety.org/wp-content/uploads/2018/02/JASA-LaTeX-v2019.zip',
+            fname)
+
+
+    if '{{ cookiecutter.presentation }}' != 'y':
+        shutil.rmtree(os.path.join(
+            PROJECT_DIRECTORY, 'process', 'presentation'))
+        shutil.rmtree(os.path.join(
+            PROJECT_DIRECTORY, 'presentation'))
+
+    if 'Not' in '{{ cookiecutter.code_license }}':
+        remove_file('LICENSE')
